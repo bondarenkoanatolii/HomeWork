2b9dbefb3f91445a90c727e9487f3026054c3c07
Bondarenko Anatolii <bondkrkut@gmail.com>
2022-12-14 21:00:16 +0200

    Homework 13. Аннотации. Рефлексия.

---
+++ src/com/hillel/anatoliibondarenko/homework13/TestRunnerException.java
@@ -0,0 +1,8 @@
+package com.hillel.anatoliibondarenko.homework13;
+
+public class TestRunnerException extends RuntimeException {
+
+    public TestRunnerException(String message) {
+        super(message);
+    }
+}
